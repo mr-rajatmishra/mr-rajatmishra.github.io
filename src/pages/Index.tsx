51a4f5b74ef9e4c16a0707524ce20a774d7d5d53
RAJAT MISHRA <mrajat099@gmail.com>
2026-04-10 03:48:48 +0530

    new

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,13 +1,13 @@
-import Navbar from '@/components/Navbar';
-import HeroSection from '@/components/HeroSection';
-import AboutSection from '@/components/AboutSection';
-import ServicesSection from '@/components/ServicesSection';
-import TechServicesSection from '@/components/TechServicesSection';
-import IndustriesSection from '@/components/IndustriesSection';
-import TeamSection from '@/components/TeamSection';
-import ClientsSection from '@/components/ClientsSection';
-import ContactSection from '@/components/ContactSection';
-import Footer from '@/components/Footer';
+import Navbar from '../components/Navbar';
+import HeroSection from '../components/HeroSection';
+import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
+import TechServicesSection from '../components/TechServicesSection';
+import IndustriesSection from '../components/IndustriesSection';
+import TeamSection from '../components/TeamSection';
+import ClientsSection from '../components/ClientsSection';
+import ContactSection from '../components/ContactSection';
+import Footer from '../components/Footer';
 
 const Index = () => (
   <div className="min-h-screen overflow-x-hidden">
